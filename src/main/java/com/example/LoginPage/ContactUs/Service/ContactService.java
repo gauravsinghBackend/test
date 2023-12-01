5231d6f394a52aae430ad9e1d4d7ddb762b877fe
shreyanshZevo <shreyansh.rathore@zevo360.com>
2023-12-01 12:58:10 +0530

    Update: Contact us part-2

--- src/main/java/com/example/LoginPage/ContactUs/Service/ContactService.java
+++ src/main/java/com/example/LoginPage/ContactUs/Service/ContactService.java
@@ -2,7 +2,9 @@ package com.example.LoginPage.ContactUs.Service;
 
 
 import com.example.LoginPage.ContactUs.Dto.ContactDto;
+import com.example.LoginPage.ContactUs.Dto.PostContactDto;
 import com.example.LoginPage.ContactUs.Entity.ContactEntity;
+import com.example.LoginPage.ContactUs.Entity.PostContactEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,6 @@ import java.util.List;
 @Component
 public interface ContactService {
 
-ResponseEntity<List<ContactEntity>>getContact();
-ResponseEntity<String>postContact(ContactDto contactDto);
+    ResponseEntity<List<ContactEntity>>getContact();
+    ResponseEntity<PostContactEntity>postLoginContactUs(PostContactDto postContactDto);
 }
